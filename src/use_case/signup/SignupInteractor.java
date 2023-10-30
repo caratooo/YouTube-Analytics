b5074ac8399a4cd42007ab33e99849fedcaa9bb9
AzkyT <alextran.quandinh@gmail.com>
2023-10-30 19:47:59 -0400

    Updated all signup and login use case classes, added all necessary attributes and methods

--- src/use_case/signup/SignupInteractor.java
+++ src/use_case/signup/SignupInteractor.java
@@ -1,4 +1,37 @@
 package use_case.signup;
 
-public class SignupInteractor {
+import entities.User;
+import entities.UserFactory;
+
+import java.time.LocalDateTime;
+
+public class SignupInteractor implements SignupInputBoundary{
+    final SignupDataAccessInterface userDataAccessObject;
+    final SignupOutputBoundary userPresenter;
+    final UserFactory userFactory;
+
+    public SignupInteractor(SignupDataAccessInterface signupDataAccessInterface,
+                            SignupOutputBoundary signupOutputBoundary,
+                            UserFactory userFactory) {
+        this.userDataAccessObject = signupDataAccessInterface;
+        this.userPresenter = signupOutputBoundary;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(SignupInputData signupInputData) {
+        if (userDataAccessObject.existsByName(signupInputData.getUsername())) {
+            userPresenter.prepareFailView("User already exists.");
+        } else if (!signupInputData.getPassword().equals(signupInputData.getRepeatPassword())) {
+            userPresenter.prepareFailView("Passwords don't match.");
+        } else {
+            LocalDateTime now = LocalDateTime.now();
+
+            User user = userFactory.create(signupInputData.getUsername(), signupInputData.getPassword());
+            userDataAccessObject.save(user);
+
+            SignupOutputData signupOutputData = new SignupOutputData(user.getName(), now.toString(), false);
+            userPresenter.prepareSuccessView(signupOutputData);
+        }
+    }
 }
