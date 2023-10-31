7eff136d7230398007c0f9ead4e83a221b55fc0a
AzkyT <alextran.quandinh@gmail.com>
2023-10-30 20:00:19 -0400

    Implemented the data access interfaces from each use case, and added and implemented the required methods so far

--- src/data_access/FileUserDataAccess.java
+++ src/data_access/FileUserDataAccess.java
@@ -1,4 +1,96 @@
 package data_access;
 
-public class FileUserDataAccess {
+import entities.User;
+import entities.UserFactory;
+import use_case.channel_search.ChannelSearchDataAccessInterface;
+import use_case.compare_videos.CompareDataAccessInterface;
+import use_case.history.HistoryDataAccessInterface;
+import use_case.login.LoginDataAccessInterface;
+import use_case.signup.SignupDataAccessInterface;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FileUserDataAccess implements SignupDataAccessInterface, LoginDataAccessInterface, HistoryDataAccessInterface, CompareDataAccessInterface, ChannelSearchDataAccessInterface {
+    private final File csvFile;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    private final Map<String, User> accounts = new HashMap<>();
+
+    private UserFactory userFactory;
+
+    public FileUserDataAccess(String csvPath, UserFactory userFactory) throws IOException {
+        this.userFactory = userFactory;
+
+        csvFile = new File(csvPath);
+        headers.put("username", 0);
+        headers.put("password", 1);
+
+        if (csvFile.length() == 0) {
+            save();
+        } else {
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
+
+                // For later: clean this up by creating a new Exception subclass and handling it in the UI.
+                assert header.equals("username,password");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String password = String.valueOf(col[headers.get("password")]);
+                    User user = userFactory.create(username, password);
+                    accounts.put(username, user);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void save(User user) {
+        accounts.put(user.getName(), user);
+        this.save();
+    }
+
+    @Override
+    public User get(String username) {
+        return accounts.get(username);
+    }
+
+    private void save() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(csvFile));
+            writer.write(String.join(",", headers.keySet()));
+            writer.newLine();
+
+            for (User user : accounts.values()) {
+                String line = String.format("%s,%s",
+                        user.getName(), user.getPassword());
+                writer.write(line);
+                writer.newLine();
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+    @Override
+    public boolean existsByName(String identifier) {
+        return accounts.containsKey(identifier);
+    }
 }
