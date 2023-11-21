96dec3850fa6f6c7a206381f76b1f0decb8d3ef3
cynthia <cyn.zhou@mail.utoronto.ca>
2023-11-21 03:19:38 -0500

    - Completed instruction button in HomeView. (Detailed texts on the instruction panel may need further development) - Called new View() for VideoSearch, ChannelSearch, Trending, Compare and History.

--- src/views/HomeView.java
+++ src/views/HomeView.java
@@ -42,86 +42,106 @@ public class HomeView extends JPanel implements ActionListener, PropertyChangeLi
         instruction = new JButton(HomeViewModel.INSTRUCTION_BUTTON_LABEL);
         buttons.add(instruction);
 
-//        searchVideo.addActionListener(
-//                // This creates an anonymous subclass of ActionListener and instantiates it.
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(searchVideo)) {
-//                            VideoSearchState currentState = VideoSearchViewModel.getState();
-//                            VideoSearchController.execute();
-//                            // TODO
-//                        }
-//                    }
-//                }
-//        );
-//
-//        searchChannel.addActionListener(
-//                // This creates an anonymous subclass of ActionListener and instantiates it.
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(searchChannel)) {
-//                            ChannelSearchState currentState = ChannelSearchViewModel.getState();
-//                            ChannelSearchController.execute();
-//                            // TODO
-//                        }
-//                    }
-//                }
-//        );
-//
-//        trending.addActionListener(
-//                // This creates an anonymous subclass of ActionListener and instantiates it.
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(trending)) {
-//                            TrendingState currentState = TrendingViewModel.getState();
-//                            TrendingController.execute();
-//                            // TODO
-//                        }
-//                    }
-//                }
-//        );
-//
-//        compare.addActionListener(
-//                // This creates an anonymous subclass of ActionListener and instantiates it.
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(compare)) {
-//                            CompareState currentState = CompareViewModel.getState();
-//                            CompareController.execute();
-//                            // TODO
-//                        }
-//                    }
-//                }
-//        );
-//
-//        history.addActionListener(
-//                // This creates an anonymous subclass of ActionListener and instantiates it.
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(history)) {
-//                            HistoryState currentState = HistoryViewModel.getState();
-//                            HistoryController.execute();
-//                            // TODO
-//                        }
-//                    }
-//                }
-//        );
-//
-//        instruction.addActionListener(
-//                // This creates an anonymous subclass of ActionListener and instantiates it.
-//                new ActionListener() {
-//                    public void actionPerformed(ActionEvent evt) {
-//                        if (evt.getSource().equals(instruction)) {
-//                            // TODO
-//                        }
-//                    }
-//                }
-//        );
+        searchVideo.addActionListener(
+//                 This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(searchVideo)) {
+                            new VideoSearchView();
+                        }
+                    }
+                }
+        );
+
+        searchChannel.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(searchChannel)) {
+                            new ChannelSearchView();
+                        }
+                    }
+                }
+        );
+
+        trending.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(trending)) {
+                            new TrendingView();
+                        }
+                    }
+                }
+        );
+
+        compare.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(compare)) {
+                            new CompareView();
+                        }
+                    }
+                }
+        );
+
+        history.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(history)) {
+                            new HistoryView();
+                        }
+                    }
+                }
+        );
+
+        instruction.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent evt) {
+                        if (evt.getSource().equals(instruction)) {
+                            openInstructionPanel();
+                        }
+                    }
+                }
+        );
 
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(title);
         this.add(buttons);
+    }
+
+    private static void openInstructionPanel() {
+        // Create a new JFrame for the search video view
+        JFrame instructionFrame = new JFrame("Instructions");
+        instructionFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        instructionFrame.setSize(400, 300);
+
+        // Create a panel for the search video view
+        JPanel instructionPanel = new JPanel(new GridLayout(0, 1));
+
+        // Sample instructions
+        String[] instructions = {
+                "Search Video: Type in Youtube Video ID in the search bar to see its stats.",
+                "Search Channel: Type in Youtube Channel ID in the search bar to see its stats.",
+                "Trending: Search most popular videos in general or by categories.",
+                "Compare: Compare statistics between two videos.",
+                "History: Show 5 most recent search history."
+        };
+
+        // Add instructions to the panel
+        for (String instruction : instructions) {
+            JLabel instructionLabel = new JLabel(instruction);
+            instructionPanel.add(instructionLabel);
+        }
+        // Set the search panel as the content pane of the search frame
+        instructionFrame.setContentPane(instructionPanel);
+
+        // Display the search frame
+        instructionFrame.setVisible(true);
     }
 
     @Override
