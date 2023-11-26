2e03a6f37b64bbe3fce6e34296bc98506e5cb5af
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 12:32:06 -0500

    finished VideoSearchOutputBoundary implementation

--- src/use_case/video_search/VideoSearchOutputBoundary.java
+++ src/use_case/video_search/VideoSearchOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.video_search;
 
 public interface VideoSearchOutputBoundary {
+
+    void prepareSuccessView(VideoSearchOutputData video);
+
+    void prepareFailView(String error);
+
 }
