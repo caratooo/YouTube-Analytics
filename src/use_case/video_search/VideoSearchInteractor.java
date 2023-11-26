2e8e3bff9ee2cf44b00874163cac6f5f61dc3239
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 12:30:33 -0500

    finished VideoSearchInteractor implementation - errors since classes aren't implemented yet

--- src/use_case/video_search/VideoSearchInteractor.java
+++ src/use_case/video_search/VideoSearchInteractor.java
@@ -1,4 +1,37 @@
 package use_case.video_search;
 
-public class VideoSearchInteractor {
+//import java.time.LocalDateTime;
+
+public class VideoSearchInteractor implements VideoSearchInputBoundary {
+
+    final VideoSearchDataAccessInterface videoSearchDataAccessObject;
+
+    final VideoSearchOutputBoundary videoPresenter;
+
+    // TODO design pattern???? (see sign up's factory in SignupInteractor)
+
+    public VideoSearchInteractor(VideoSearchDataAccessInterface videoSearchDataAccessInterface,
+                                 VideoSearchOutputBoundary videoSearchOutputBoundary) {
+        this.videoSearchDataAccessObject = videoSearchDataAccessInterface;
+        this.videoPresenter = videoSearchOutputBoundary;
+    }
+
+
+    @Override
+    public void execute(VideoSearchInputData videoSearchInputData) {
+        if (videoSearchDataAccessObject.idInvalid(videoSearchInputData.getVideoId())) {
+            videoPresenter.prepareFailView("Video ID does not exist.");
+        } else {
+
+//            LocalDateTime now = LocalDateTime.now();
+
+            Video video = videoFactory.create(videoSearchInputData.getVideoId());
+
+            videoSearchDataAccessObject.save(video);  // TODO this DAO or another that does the history stuff?
+
+            VideoSearchOutputData videoSearchOutputData = new VideoSearchOutputData(video.getName(), video.getStats(), false);
+            videoPresenter.prepareSuccessView(videoSearchOutputData);
+
+        }
+    }
 }
