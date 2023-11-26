7395d6926dbb8ddc2f6ee3125b31fb5c9ae9449f
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 13:02:14 -0500

    finished VideoSearchDataAccessInterface - TODOs are maybe change later

--- src/use_case/video_search/VideoSearchDataAccessInterface.java
+++ src/use_case/video_search/VideoSearchDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.video_search;
 
+import entities.Video;
+
 public interface VideoSearchDataAccessInterface {
+
+    boolean idInvalid(String identifier);
+
+    void save(Video video);  // TODO see VideoSearchInteractor 32
+
 }
