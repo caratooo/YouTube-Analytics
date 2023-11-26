c2147d73b90cb8950866d34f27be6b27ea5973b8
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 11:44:58 -0500

    finished VideoSearchViewModel implementation (TODO is a maybe need to implement)

--- src/interface_adapter/video_search/VideoSearchViewModel.java
+++ src/interface_adapter/video_search/VideoSearchViewModel.java
@@ -1,4 +1,36 @@
 package interface_adapter.video_search;
 
-public class VideoSearchViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class VideoSearchViewModel extends ViewModel {
+
+    public static final String TITLE_LABEL = "Video Search View";
+    public static final String VIDEO_ID_LABEL = "Input a video ID";
+    public static final String SEARCH_BUTTON_LABEL = "Search";
+
+    private VideoSearchState state = new VideoSearchState();
+
+    public VideoSearchViewModel() { super("video search"); }
+
+    public void setState(VideoSearchState state) { this.state = state; }
+
+    public VideoSearchState getState() { return state; }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+
+    // TODO get user??? (see LoggedInViewModel)
+
+
 }
