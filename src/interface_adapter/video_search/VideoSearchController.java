1576a2f9bcf3ed6dced3f97bde23bbea49179702
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 12:06:58 -0500

    finished VideoSearchController implementation - may have errors as Interactor and InputData have yet to be implemented - though, SearchView should be good now (besides TODOs)

--- src/interface_adapter/video_search/VideoSearchController.java
+++ src/interface_adapter/video_search/VideoSearchController.java
@@ -1,4 +1,21 @@
 package interface_adapter.video_search;
 
+import use_case.video_search.VideoSearchInputBoundary;
+import use_case.video_search.VideoSearchInputData;
+
 public class VideoSearchController {
+
+    final VideoSearchInputBoundary videoUseCaseInteractor;
+
+    public VideoSearchController(VideoSearchInputBoundary videoUseCaseInteractor) {
+        this.videoUseCaseInteractor = videoUseCaseInteractor;
+    }
+
+    public void execute(String videoId) {
+        VideoSearchInputData videoSearchInputData = new VideoSearchInputData(videoId);
+
+        videoUseCaseInteractor.execute(videoSearchInputData);
+    }
+
+
 }
