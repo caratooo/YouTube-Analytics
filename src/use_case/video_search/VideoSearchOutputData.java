8a86a574fee6008fd3a31de6f0d9a3f4f0720e12
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 12:39:51 -0500

    finished VideoSearchOutputData implementation - may require changes depending on DAO (see TODOs)

--- src/use_case/video_search/VideoSearchOutputData.java
+++ src/use_case/video_search/VideoSearchOutputData.java
@@ -1,4 +1,27 @@
 package use_case.video_search;
 
 public class VideoSearchOutputData {
+
+    private final String videoId;
+
+    private final String videoAbout;  // TODO see below
+
+    private final String videoStats;  // TODO not sure if it's a string (might be a dictionary/hashmap)
+
+    private boolean useCaseFailed;
+
+    public VideoSearchOutputData (String videoId, String videoAbout, String videoStats, boolean useCaseFailed) {
+        this.videoId = videoId;
+        this.videoAbout = videoAbout;
+        this.videoStats = videoStats;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String getVideoId() { return videoId; }
+
+    public String getVideoAbout() { return videoAbout; }
+
+    public String getVideoStats() { return videoStats; }
+
+
 }
