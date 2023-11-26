f019d9e2cc1e1c2a1ec6a610a21d62ad3e940bf1
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-26 12:10:32 -0500

    finished VideoSearchInputData implementation - Controller should be good now

--- src/use_case/video_search/VideoSearchInputData.java
+++ src/use_case/video_search/VideoSearchInputData.java
@@ -1,4 +1,13 @@
 package use_case.video_search;
 
 public class VideoSearchInputData {
+
+    final private String videoId;
+
+    public VideoSearchInputData(String videoId) {
+        this.videoId = videoId;
+    }
+
+    String getVideoId() { return videoId; }
+
 }
