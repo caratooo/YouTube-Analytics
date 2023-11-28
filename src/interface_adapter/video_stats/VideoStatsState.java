a9c1159c2671ea00ad784b415132e94fea58485b
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-27 20:54:42 -0500

    updated VideoStatsState to match Video entity attributes

--- src/interface_adapter/video_stats/VideoStatsState.java
+++ src/interface_adapter/video_stats/VideoStatsState.java
@@ -1,42 +1,85 @@
 package interface_adapter.video_stats;
 
+import com.google.api.client.util.DateTime;
+
 public class VideoStatsState {
 
     private String videoId = "";
 
-    private String stats = "";
+    private String channelName = "";
 
-    private String about = "";
+    private String title = "";
+
+    private String description = "";
+
+    // TODO default datetime
+    private DateTime videoPublishDate = new DateTime("December 10");
+
+    private int viewCount = 0;
+
+    private int likeCount = 0;
+
+    private int commentCount = 0;
 
     public VideoStatsState(VideoStatsState copy) {
         videoId = copy.videoId;
-        stats = copy.stats;
-        about = copy.about;
+        channelName = copy.channelName;
+        title = copy.title;
+        description = copy.description;
+        videoPublishDate = copy.videoPublishDate;
+        viewCount = copy.viewCount;
+        likeCount = copy.likeCount;
+        commentCount = copy.commentCount;
     }
 
     public VideoStatsState() {}
 
-    public String getVideoId() {
-        return this.videoId;
-    }
+    public String getVideoId() { return videoId; }
 
-    public String getVideoStats() {
-        return this.stats;
-    }
+    public String getChannelName() { return channelName; }
 
-    public String getVideoAbout() {
-        return this.about;
-    }
+    public String getTitle() { return title; }
+
+    public String getDescription() { return description; }
+
+    public DateTime getVideoPublishDate() { return videoPublishDate; }
+
+    public int getViewCount() { return viewCount; }
+
+    public int getLikeCount() { return likeCount; }
+
+    public int getCommentCount() {return commentCount; }
 
     public void setVideoId(String videoId) {
         this.videoId = videoId;
     }
 
-    public void setVideoStats(String videoStats) {
-        this.stats = videoStats;
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
     }
 
-    public void setVideoAbout(String videoAbout) {
-        this.about = videoAbout;
+    public void setTitle(String title) {
+        this.title = title;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setVideoPublishDate(DateTime videoPublishDate) {
+        this.videoPublishDate = videoPublishDate;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public void setCommentCount(int likeCount) {
+        this.commentCount = commentCount;
+    }
+
 }
