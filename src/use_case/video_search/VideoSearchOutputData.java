4ece03ac0756669f41b9de9fdf6653390ac1e9fb
caratooo <107289876+caratooo@users.noreply.github.com>
2023-11-27 20:16:29 -0500

    updated VideoSearchOutputData to match Video attributes

--- src/use_case/video_search/VideoSearchOutputData.java
+++ src/use_case/video_search/VideoSearchOutputData.java
@@ -1,27 +1,56 @@
 package use_case.video_search;
 
+import com.google.api.client.util.DateTime;
+
 public class VideoSearchOutputData {
 
     private final String videoId;
 
-    private final String videoAbout;  // TODO see below
+    private final String channelName;
 
-    private final String videoStats;  // TODO not sure if it's a string (might be a dictionary/hashmap)
+    private final String title;
+
+    private final String description;
+
+    private final DateTime videoPublishDate;
+
+    private final int viewCount;
+
+    private final int likeCount;
+
+    private final int commentCount;
 
     private boolean useCaseFailed;
 
-    public VideoSearchOutputData (String videoId, String videoAbout, String videoStats, boolean useCaseFailed) {
+    public VideoSearchOutputData (String videoId, String channelName, String title,
+                                  String description, DateTime videoPublishDate,
+                                  int viewCount, int likeCount, int commentCount, boolean useCaseFailed) {
         this.videoId = videoId;
-        this.videoAbout = videoAbout;
-        this.videoStats = videoStats;
+        this.channelName = channelName;
+        this.title = title;
+        this.description = description;
+        this.videoPublishDate = videoPublishDate;
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+        this.commentCount = commentCount;
         this.useCaseFailed = useCaseFailed;
     }
 
     public String getVideoId() { return videoId; }
 
-    public String getVideoAbout() { return videoAbout; }
+    public String getChannelName() { return channelName; }
 
-    public String getVideoStats() { return videoStats; }
+    public String getTitle() { return title; }
+
+    public String getDescription() { return description; }
+
+    public DateTime getVideoPublishDate() { return videoPublishDate; }
+
+    public int getViewCount() { return viewCount; }
+
+    public int getLikeCount() { return likeCount; }
+
+    public int getCommentCount() {return commentCount; }
 
 
 }
