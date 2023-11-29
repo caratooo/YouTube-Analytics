8523e2ab0824b95b76ef51fe30de353cc31090e5
AzkyT <alextran.quandinh@gmail.com>
2023-11-29 14:52:47 -0500

    implemented a rudimentary HistoryState class

--- src/interface_adapter/history/HistoryState.java
+++ src/interface_adapter/history/HistoryState.java
@@ -1,4 +1,37 @@
 package interface_adapter.history;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HistoryState {
+    private String username = "";
+    private List<String> userHistory = new ArrayList<>();
+    private String userHistoryError = null;
+
+    public HistoryState() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<String> getUserHistory() {
+        return userHistory;
+    }
+
+    public String getUserHistoryError() {
+        return userHistoryError;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserHistory(List<String> userHistory) {
+        this.userHistory = userHistory;
+    }
+
+    public void setUserHistoryError(String userHistoryError) {
+        this.userHistoryError = userHistoryError;
+    }
 }
