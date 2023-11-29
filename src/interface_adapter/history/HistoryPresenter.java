150b0e15c80f26f4fd0c0447673736645c6018be
AzkyT <alextran.quandinh@gmail.com>
2023-11-29 14:54:46 -0500

    implemented the outline for HistoryPresenter, prepareSuccessView not implemented

--- src/interface_adapter/history/HistoryPresenter.java
+++ src/interface_adapter/history/HistoryPresenter.java
@@ -1,4 +1,19 @@
 package interface_adapter.history;
 
-public class HistoryPresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.history.HistoryOutputBoundary;
+import use_case.history.HistoryOutputData;
+
+public class HistoryPresenter implements HistoryOutputBoundary {
+    private final HistoryViewModel historyViewModel;
+    private ViewManagerModel viewManagerModel;
+
+
+    public HistoryPresenter(HistoryViewModel historyViewModel, ViewManagerModel viewManagerModel) {
+        this.historyViewModel = historyViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+    @Override
+    public void prepareSuccessView(HistoryOutputData historyOutputData) {
+    }
 }
