e5a0ccbd076aba7b8a1773754d524e3be982c748
AzkyT <alextran.quandinh@gmail.com>
2023-11-29 14:55:13 -0500

    implemented the outline for HistoryView, no methods implemented

--- src/views/HistoryView.java
+++ src/views/HistoryView.java
@@ -1,4 +1,36 @@
 package views;
 
-public class HistoryView {
+import interface_adapter.history.HistoryController;
+import interface_adapter.history.HistoryViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class HistoryView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "history";
+    private final HistoryViewModel historyViewModel;
+    private final HistoryController historyController;
+
+    public HistoryView(HistoryController historyController, HistoryViewModel historyViewModel) {
+        this.historyController = historyController;
+        this.historyViewModel = historyViewModel;
+        historyViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel(HistoryViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
