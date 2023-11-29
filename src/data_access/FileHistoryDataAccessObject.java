cc88b5f660e0af8b80e8869c4581a4937ccf5805
AzkyT <alextran.quandinh@gmail.com>
2023-11-29 14:52:05 -0500

    added the history DAO, with the saveUserHistory method finished but not tested

---
+++ src/data_access/FileHistoryDataAccessObject.java
@@ -0,0 +1,95 @@
+package data_access;
+
+import use_case.history.HistoryDataAccessInterface;
+
+import java.io.*;
+import java.util.*;
+
+public class FileHistoryDataAccessObject implements HistoryDataAccessInterface {
+    private final Map<String, File> csvFileHistories = new HashMap<>();
+    private final Map<String, List<String>> usersHistories = new HashMap<>();
+    private final Map<String, String[]> headersHistories = new HashMap<>();
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    public FileHistoryDataAccessObject(FileUserDataAccessObject userDataAccessObject) throws FileNotFoundException { // add to FileUserDataAccessObject
+        headers.put("type", 0);
+        headers.put("output", 1);
+
+        headersHistories.put("videoSearch",
+                new String[]{"1", "2", "3", "4", "5", "6", "7"});
+        headersHistories.put("compare",
+                new String[]{"1", "2", "3", "4", "5", "6", "7"});
+
+        if (!userDataAccessObject.isEmpty()) {
+            Object[] users = userDataAccessObject.getUsers();
+
+            for (Object user : users) {
+                if (!doesUserFileExist((String)user)) {
+                    continue;
+                }
+
+                String csvPath = String.format("./%sHistory.csv", user);
+                File userFile = new File(csvPath);
+                csvFileHistories.put((String) user, userFile);
+
+                BufferedReader reader = new BufferedReader(new FileReader(userFile));
+                try {
+                    String row;
+                    usersHistories.put((String) user, new ArrayList<>());
+                    List<String> userHistory = usersHistories.get(user);
+
+                    while ((row = reader.readLine()) != null) {
+                        userHistory.add(row);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public void saveUserHistory(String identifier, String listOfData) {
+        usersHistories.get(identifier).add(listOfData);
+
+        if (!doesUserFileExist(identifier)) {
+            String csvPath = String.format("./%sHistory.csv", identifier);
+            File userFile = new File(csvPath);
+            csvFileHistories.put(identifier, userFile);
+            save(userFile);
+        } else {
+            File userFile = csvFileHistories.get(identifier);
+            save(userFile);
+        }
+    }
+
+    public void save(File userFile) {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(userFile));
+            writer.write(String.join(",", headersHistories.keySet()));
+            writer.newLine();
+
+            for (String user : usersHistories.keySet()) {
+                List<String> userHistory = usersHistories.get(user);
+
+                for (String query : userHistory) {
+                    writer.write(query);
+                    writer.newLine();
+                }
+            }
+
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean doesUserFileExist(String identifier) {
+        return csvFileHistories.containsKey(identifier);
+    }
+    @Override
+    public List<String> getUserHistory(String identifier) {
+        return usersHistories.get(identifier);
+    }
+}
