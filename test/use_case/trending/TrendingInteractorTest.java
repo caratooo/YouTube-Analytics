9857cc1043446f471d2040abb8dd567fede9bea6
cynthia <cyn.zhou@mail.utoronto.ca>
2023-11-30 14:59:27 -0500

    - added trendingInteractorTest file

---
+++ test/use_case/trending/TrendingInteractorTest.java
@@ -0,0 +1,18 @@
+package use_case.trending;
+
+import data_access.YouTubeDataAccess;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrendingInteractorTest {
+    @Test
+    void successTest() {
+        TrendingInputData inputdata = new TrendingInputData("10");
+        // YouTubeDataAccess youTubeDataAccess = new TestingYouTubeDataAccess();
+
+
+
+    }
+
+}
