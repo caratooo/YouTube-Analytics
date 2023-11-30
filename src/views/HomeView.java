66560b344000ca01d5f688f32f638eb5d455e0a9
cynthia <cyn.zhou@mail.utoronto.ca>
2023-11-30 00:02:04 -0500

    - imported instructionView from views to add instruction view

--- src/views/HomeView.java
+++ src/views/HomeView.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
+import static views.InstructionView.openInstructionPanel;
+
 public class HomeView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "home";
     private final HomeViewModel homeViewModel;
@@ -112,36 +114,6 @@ public class HomeView extends JPanel implements ActionListener, PropertyChangeLi
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(title);
         this.add(buttons);
-    }
-
-    private static void openInstructionPanel() {
-        // Create a new JFrame for the search video view
-        JFrame instructionFrame = new JFrame("Instructions");
-        instructionFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        instructionFrame.setSize(400, 300);
-
-        // Create a panel for the search video view
-        JPanel instructionPanel = new JPanel(new GridLayout(0, 1));
-
-        // Sample instructions
-        String[] instructions = {
-                "Search Video: Type in Youtube Video ID in the search bar to see its stats.",
-                "Search Channel: Type in Youtube Channel ID in the search bar to see its stats.",
-                "Trending: Search most popular videos in general or by categories.",
-                "Compare: Compare statistics between two videos.",
-                "History: Show 5 most recent search history."
-        };
-
-        // Add instructions to the panel
-        for (String instruction : instructions) {
-            JLabel instructionLabel = new JLabel(instruction);
-            instructionPanel.add(instructionLabel);
-        }
-        // Set the search panel as the content pane of the search frame
-        instructionFrame.setContentPane(instructionPanel);
-
-        // Display the search frame
-        instructionFrame.setVisible(true);
     }
 
     @Override
