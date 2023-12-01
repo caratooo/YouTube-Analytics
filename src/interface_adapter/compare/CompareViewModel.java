e94a4219ab35d5f7944ac4b070e04a16db72523d
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-01 17:50:32 -0500

    initializing CompareViewModel with imports and extension dependency

--- src/interface_adapter/compare/CompareViewModel.java
+++ src/interface_adapter/compare/CompareViewModel.java
@@ -1,4 +1,22 @@
 package interface_adapter.compare;
 
-public class CompareViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class CompareViewModel extends ViewModel {
+    public CompareViewModel() {
+        super("compare");
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
 }
