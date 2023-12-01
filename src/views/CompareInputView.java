77edfca1bf59fb54265efef923a3015465dbc5b7
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-01 17:39:28 -0500

    added imports needed to implement this view

--- src/views/CompareInputView.java
+++ src/views/CompareInputView.java
@@ -1,4 +1,20 @@
 package views;
 
+import interface_adapter.ViewManagerModel;
+import interface_adapter.home.HomeViewModel;
+import interface_adapter.video_search.VideoSearchController;
+import interface_adapter.video_search.VideoSearchState;
+import interface_adapter.video_search.VideoSearchViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 public class CompareInputView {
 }
