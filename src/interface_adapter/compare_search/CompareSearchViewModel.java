4e2ded8e8c01afed42c5b568fe4541241529fcb1
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-01 17:57:48 -0500

    Updated required methods inside of the View Model

--- src/interface_adapter/compare_search/CompareSearchViewModel.java
+++ src/interface_adapter/compare_search/CompareSearchViewModel.java
@@ -6,7 +6,15 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class CompareSearchViewModel extends ViewModel {
+
+    public static final String TITLE_LABEL = "Compare Videos Search View";
+    public static final String VIDEO_ID_LABEL = "Input a video ID";
+    public static final String SEARCH_BUTTON_LABEL = "Compare";
+    public static final String BACK_BUTTON_LABEL = "Back to Home";
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private CompareSearchState state = new CompareSearchState();
+
+    public void setState(CompareSearchState state) { this.state = state; }
     public CompareSearchViewModel() {
         super("compare");
     }
