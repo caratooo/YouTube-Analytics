6222d8a4d2b4b2cfe5c7c0d7d67d7df2af7426f6
AzkyT <alextran.quandinh@gmail.com>
2023-12-01 17:38:27 -0500

    implemented the sorting algorithm for a video search panel to add unto the history view

--- src/views/sort_algorithms/SortVideoSearch.java
+++ src/views/sort_algorithms/SortVideoSearch.java
@@ -6,23 +6,48 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SortVideoSearch implements SortSearchQuery {
-    private static final List<String> statLabels = Arrays.asList("title", "2", "3", "4", "5", "6", "7");
+
+    private static final Color grey = new Color(227, 227, 227);
+    private static final FlowLayout flowlayout = new FlowLayout(FlowLayout.CENTER);
+    private static final List<String> statLabels = Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8");
+
     @Override
     public JPanel sort(String data, Integer number) {
-        JPanel main = new JPanel(new FlowLayout(FlowLayout.CENTER));
-
-        JPanel pLabel = new JPanel();
-        JLabel label = new JLabel(String.);
-
-        JPanel left = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel centre = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel right = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel main = new JPanel(flowlayout);
 
         List<String> stats = List.of(data.split(","));
 
-        for (int i = 0; i < 7; i++) {
+        JPanel pLabel = new JPanel();
+        JLabel label = new JLabel(String.format("Search %s: Video (%s)", number, stats.get(0)));
+        pLabel.add(label);
 
+        JPanel left = new JPanel(flowlayout);
+        JPanel centre = new JPanel(flowlayout);
+        JPanel right = new JPanel(flowlayout);
+
+        for (int i = 0; i < 7; i++) {
+            JLabel new_stat = new JLabel(String.format("%s: %s", statLabels.get(i), stats.get(i)));
+            if (i < 3) {
+                left.add(new_stat);
+            } else if (i < 6) {
+                centre.add(new_stat);
+            } else {
+                right.add(new_stat);
+            }
         }
+
+        if (number % 2 != 0) {
+            pLabel.setBackground(grey);
+            left.setBackground(grey);
+            centre.setBackground(grey);
+            right.setBackground(grey);
+            main.setBackground(grey);
+        }
+
+        main.add(pLabel);
+        main.add(left);
+        main.add(centre);
+        main.add(right);
 
         return main;
     }
