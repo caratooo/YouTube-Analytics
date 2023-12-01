02e77a7180c5482323a84ea63562d0fed71d0f3e
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-01 18:18:30 -0500

    Initializing CompareSearchState

--- src/interface_adapter/compare_search/CompareSearchState.java
+++ src/interface_adapter/compare_search/CompareSearchState.java
@@ -1,4 +1,40 @@
 package interface_adapter.compare_search;
 
 public class CompareSearchState {
+
+    private String videoOneId = "";
+    private String videoTwoId = "";
+    private String videoIdError = null;
+
+    public CompareSearchState(CompareSearchState copy){
+        videoOneId = copy.videoOneId;
+        videoTwoId = copy.videoTwoId;
+    }
+    public CompareSearchState(){
+
+    }
+    public String getVideoOneId() {
+        return videoOneId;
+    }
+
+    public void setVideoOneId(String videoOneId) {
+        this.videoOneId = videoOneId;
+    }
+
+    public String getVideoTwoId() {
+        return videoTwoId;
+    }
+
+    public void setVideoTwoId(String videoTwoId) {
+        this.videoTwoId = videoTwoId;
+    }
+
+    public String getVideoIdError() {
+        return videoIdError;
+    }
+
+    public void setVideoIdError(String videoIdError) {
+        this.videoIdError = videoIdError;
+    }
+
 }
