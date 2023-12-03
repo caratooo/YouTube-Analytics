af05b11efbdc71b82728f7d878f5571e33a39ab8
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-03 12:31:52 -0500

    fixed variable names

--- src/use_case/compare_videos/CompareSearchInteractor.java
+++ src/use_case/compare_videos/CompareSearchInteractor.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class CompareSearchInteractor implements CompareSearchInputBoundary {
-    final CompareSearchDataAccessInterface videoSearchDataAccessObject;
+    final CompareSearchDataAccessInterface compareSearchDataAccessObject;
     final CompareSearchOutputBoundary compareSearchPresenter;
 
-    public CompareSearchInteractor(CompareSearchDataAccessInterface videoSearchDataAccessObject, CompareSearchOutputBoundary compareSearchPresenter) {
-        this.videoSearchDataAccessObject = videoSearchDataAccessObject;
+    public CompareSearchInteractor(CompareSearchDataAccessInterface compareSearchDataAccessObject, CompareSearchOutputBoundary compareSearchPresenter) {
+        this.compareSearchDataAccessObject = compareSearchDataAccessObject;
         this.compareSearchPresenter = compareSearchPresenter;
     }
 
