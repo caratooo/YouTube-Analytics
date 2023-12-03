d78e49a00bb140331f532a548a382333c8eda8c1
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-03 13:11:28 -0500

    corrected button actions; differentiated between input field for video one and video two

--- src/views/CompareSearchView.java
+++ src/views/CompareSearchView.java
@@ -1,13 +1,17 @@
 package views;
 
+import interface_adapter.ViewManagerModel;
 import interface_adapter.compare_search.CompareSearchController;
 import interface_adapter.compare_search.CompareSearchState;
 import interface_adapter.compare_search.CompareSearchViewModel;
+import interface_adapter.home.HomeViewModel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
@@ -19,7 +23,8 @@ public class CompareSearchView extends JPanel implements ActionListener, Propert
 
     private final CompareSearchViewModel compareSearchViewModel;
 
-    private final JTextField videoInputField = new JTextField(15);
+    private final JTextField videoInputFieldOne = new JTextField(15);
+    private final JTextField videoInputFieldTwo = new JTextField(15);
 
     private final CompareSearchController compareSearchController;
 
@@ -27,7 +32,7 @@ public class CompareSearchView extends JPanel implements ActionListener, Propert
 
     private final JButton back;
 
-    public CompareSearchView(CompareSearchViewModel compareSearchViewModel, CompareSearchController compareSearchController) {
+    public CompareSearchView(CompareSearchViewModel compareSearchViewModel, CompareSearchController compareSearchController, HomeViewModel homeViewModel, ViewManagerModel viewManagerModel) {
         this.compareSearchViewModel = compareSearchViewModel;
         this.compareSearchController = compareSearchController;
         compareSearchViewModel.addPropertyChangeListener(this);
@@ -35,9 +40,9 @@ public class CompareSearchView extends JPanel implements ActionListener, Propert
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         LabelTextPanel videoIdInfoOne = new LabelTextPanel(
-                new JLabel(CompareSearchViewModel.VIDEO_ID_LABEL), videoInputField);
+                new JLabel(CompareSearchViewModel.VIDEO_ID_LABEL), videoInputFieldOne);
         LabelTextPanel videoIdInfoTwo = new LabelTextPanel(
-                new JLabel(CompareSearchViewModel.VIDEO_ID_LABEL), videoInputField);
+                new JLabel(CompareSearchViewModel.VIDEO_ID_LABEL), videoInputFieldTwo);
 
         JPanel buttons = new JPanel();
 
@@ -48,13 +53,6 @@ public class CompareSearchView extends JPanel implements ActionListener, Propert
         buttons.add(back);
 
         search.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource().equals(search)) {
@@ -69,7 +67,61 @@ public class CompareSearchView extends JPanel implements ActionListener, Propert
             }
         });
 
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource().equals(back)) {
+                    viewManagerModel.setActiveView(homeViewModel.getViewName());
+                    viewManagerModel.firePropertyChanged();
+                }
+            }
+        });
+        videoInputFieldOne.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        CompareSearchState currState = compareSearchViewModel.getState();
+                        String text = videoInputFieldOne.getText() + e.getKeyChar();
+                        currState.setVideoOneId(text);
+                        compareSearchViewModel.setState(currState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
+        videoInputFieldTwo.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        CompareSearchState currState = compareSearchViewModel.getState();
+                        String text = videoInputFieldTwo.getText() + e.getKeyChar();
+                        currState.setVideoTwoId(text);
+                        compareSearchViewModel.setState(currState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
+
+
     }
+
 
 
     @Override
