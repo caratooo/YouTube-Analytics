a4326f7977348baf86c8d92397045daf469d952b
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:56:34 -0500

    updated the interafce and its expected methods

--- src/use_case/compare_videos/CompareSearchDataAccessInterface.java
+++ src/use_case/compare_videos/CompareSearchDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.compare_videos;
 
+import entities.Video;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public interface CompareSearchDataAccessInterface {
+    Video getVideo(String videoId) throws GeneralSecurityException, IOException;
+    boolean isInvalid(String identifier) throws GeneralSecurityException, IOException;
 }
