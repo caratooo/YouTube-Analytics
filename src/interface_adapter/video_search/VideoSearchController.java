ede905f2e064062c628431845c0361b678fcae50
AzkyT <alextran.quandinh@gmail.com>
2023-12-03 00:48:25 -0500

    implemented the SearchController interface and added some placeholder code for the necessary methods

--- src/interface_adapter/video_search/VideoSearchController.java
+++ src/interface_adapter/video_search/VideoSearchController.java
@@ -1,4 +1,14 @@
 package interface_adapter.video_search;
 
-public class VideoSearchController {
+import interface_adapter.SearchController;
+
+public class VideoSearchController implements SearchController {
+    @Override
+    public void callExecute(String data) {
+        execute(data.split(",")[1]);
+    }
+
+    public void execute(String video) {
+
+    }
 }
