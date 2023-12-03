83b69124027eb3a8b8aeeab1676b6ce40229be2f
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 22:29:02 -0500

    established the implementation dependency

--- src/interface_adapter/compare_search/CompareSearchPresenter.java
+++ src/interface_adapter/compare_search/CompareSearchPresenter.java
@@ -1,4 +1,16 @@
 package interface_adapter.compare_search;
 
-public class CompareSearchPresenter {
+import use_case.compare_videos.CompareSearchOutputBoundary;
+import use_case.compare_videos.CompareSearchOutputData;
+
+public class CompareSearchPresenter implements CompareSearchOutputBoundary {
+    @Override
+    public void prepareSuccessView(CompareSearchOutputData video) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
