b7751d8beb5b0a25d2b7a7c5ac30d4d2d5d44da6
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 22:28:29 -0500

    implemented high level logic in controller

--- src/interface_adapter/compare_search/CompareSearchController.java
+++ src/interface_adapter/compare_search/CompareSearchController.java
@@ -1,10 +1,21 @@
 package interface_adapter.compare_search;
 
+import use_case.compare_videos.CompareSearchInputBoundary;
+import use_case.compare_videos.CompareSearchInputData;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class CompareSearchController {
-    public void execute(String videoOneId, String videoTwoId)throws GeneralSecurityException, IOException {
 
+    final CompareSearchInputBoundary compareSearchUseCaseInteractor;
+
+    public CompareSearchController(CompareSearchInputBoundary compareSearchUseCaseInteractor) {
+        this.compareSearchUseCaseInteractor = compareSearchUseCaseInteractor;
+    }
+
+    public void execute(String videoOneId, String videoTwoId)throws GeneralSecurityException, IOException {
+        CompareSearchInputData compareSearchInputData = new CompareSearchInputData(videoOneId, videoTwoId);
+        compareSearchUseCaseInteractor.execute(compareSearchInputData);
     }
 }
