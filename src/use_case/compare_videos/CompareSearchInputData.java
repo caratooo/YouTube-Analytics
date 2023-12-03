b0ad32b783c1fd0ed8a3a592ea541cd49538a13d
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:43:01 -0500

    added getter methods

--- src/use_case/compare_videos/CompareSearchInputData.java
+++ src/use_case/compare_videos/CompareSearchInputData.java
@@ -3,6 +3,15 @@ package use_case.compare_videos;
 public class CompareSearchInputData {
 
     final private String videoOneId;
+
+    public String getVideoOneId() {
+        return videoOneId;
+    }
+
+    public String getVideoTwoId() {
+        return videoTwoId;
+    }
+
     final private String videoTwoId;
     public CompareSearchInputData(String videoOneId, String videoTwoId) {
         this.videoOneId = videoOneId;
