c61b985040cdc80d025f1f9b2048925c0b8c6364
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:45:54 -0500

    implementing the interface

--- src/use_case/compare_videos/CompareSearchInteractor.java
+++ src/use_case/compare_videos/CompareSearchInteractor.java
@@ -1,4 +1,13 @@
 package use_case.compare_videos;
 
-public class CompareSearchInteractor {
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class CompareSearchInteractor implements CompareSearchInputBoundary {
+
+
+    @Override
+    public void execute(CompareSearchInputData compareSearchInputData) throws GeneralSecurityException, IOException {
+
+    }
 }
