dacbfe604e7b364da64d91924c45f734e5ff29b5
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:36:37 -0500

    added exceptions that execute might throw, which are consistent with common exceptions that the YouTube API throws

--- src/interface_adapter/compare_search/CompareSearchController.java
+++ src/interface_adapter/compare_search/CompareSearchController.java
@@ -1,7 +1,10 @@
 package interface_adapter.compare_search;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class CompareSearchController {
-    public void execute(String videoOneId, String videoTwoId){
+    public void execute(String videoOneId, String videoTwoId)throws GeneralSecurityException, IOException {
 
     }
 }
