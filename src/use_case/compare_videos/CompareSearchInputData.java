441f8069004d2471e5c850aaaac3c9f83d457f5e
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:42:42 -0500

    initialized CompareSearchInputData class

--- src/use_case/compare_videos/CompareSearchInputData.java
+++ src/use_case/compare_videos/CompareSearchInputData.java
@@ -1,4 +1,11 @@
 package use_case.compare_videos;
 
 public class CompareSearchInputData {
+
+    final private String videoOneId;
+    final private String videoTwoId;
+    public CompareSearchInputData(String videoOneId, String videoTwoId) {
+        this.videoOneId = videoOneId;
+        this.videoTwoId = videoTwoId;
+    }
 }
