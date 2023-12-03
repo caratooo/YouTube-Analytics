6f6c24d35fdaf75178e86739613dcb5488fceed0
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:45:28 -0500

    added execute method in the interface

--- src/use_case/compare_videos/CompareSearchInputBoundary.java
+++ src/use_case/compare_videos/CompareSearchInputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.compare_videos;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public interface CompareSearchInputBoundary {
+    void execute(CompareSearchInputData compareSearchInputData) throws GeneralSecurityException, IOException;
 }
