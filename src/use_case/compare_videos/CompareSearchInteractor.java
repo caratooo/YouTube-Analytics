a122ca487421f4fe3ee316a3f1c756bf780aad3a
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-02 21:48:35 -0500

    initializing attribtues that the interactor needs

--- src/use_case/compare_videos/CompareSearchInteractor.java
+++ src/use_case/compare_videos/CompareSearchInteractor.java
@@ -1,10 +1,18 @@
 package use_case.compare_videos;
 
+import interface_adapter.compare_search.CompareSearchPresenter;
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class CompareSearchInteractor implements CompareSearchInputBoundary {
+    final CompareSearchDataAccessInterface videoSearchDataAccessObject;
+    final CompareSearchPresenter compareSearchPresenter;
 
+    public CompareSearchInteractor(CompareSearchDataAccessInterface videoSearchDataAccessObject, CompareSearchPresenter compareSearchPresenter) {
+        this.videoSearchDataAccessObject = videoSearchDataAccessObject;
+        this.compareSearchPresenter = compareSearchPresenter;
+    }
 
     @Override
     public void execute(CompareSearchInputData compareSearchInputData) throws GeneralSecurityException, IOException {
