d3363bedcefb24a498ef6d0dd72a52de02f5c889
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-04 16:53:23 -0500

    creating usecase factory file for compare video

---
+++ src/app/CompareVideoUseCaseFactory.java
@@ -0,0 +1,18 @@
+package app;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.home.HomeViewModel;
+import interface_adapter.compare_search.CompareSearchController;
+import interface_adapter.compare_search.CompareSearchPresenter;
+import interface_adapter.compare_search.CompareSearchViewModel;
+import interface_adapter.compare_stats.CompareStatsViewModel;
+import use_case.compare_videos.CompareSearchDataAccessInterface;
+import use_case.compare_videos.CompareSearchInputBoundary;
+import use_case.compare_videos.CompareSearchInteractor;
+import use_case.compare_videos.CompareSearchOutputBoundary;
+import views.CompareSearchView;
+
+import javax.swing.*;
+import java.io.IOException;
+public class CompareVideoUseCaseFactory {
+}
