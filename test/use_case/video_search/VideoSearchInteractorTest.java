49bf8a0cd80a17d4bf02e7d8d4980e09bf2b4b4f
caratooo <107289876+caratooo@users.noreply.github.com>
2023-12-07 18:03:55 -0500

    added dao testing for VideoSearchInteractorTest

--- test/use_case/video_search/VideoSearchInteractorTest.java
+++ test/use_case/video_search/VideoSearchInteractorTest.java
@@ -1,8 +1,8 @@
 package use_case.video_search;
 
-import data_access.InMemoryHistoryDataAccessObject;
-import data_access.VideoSearchDataAccessObject;
-import data_access.YouTubeDataAccess;
+import data_access.*;
+import entities.CommonUserFactory;
+import entities.UserFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -18,7 +18,7 @@ class VideoSearchInteractorTest {
     }
 
     @Test
-    void successTest() throws IOException, GeneralSecurityException {
+    void successTestMock() throws IOException, GeneralSecurityException {
 
         // input data + data access interface
         VideoSearchInputData inputData = new VideoSearchInputData("H-v6DfxnjF8", "user");
@@ -54,7 +54,45 @@ class VideoSearchInteractorTest {
     }
 
     @Test
-    void failureTest() throws IOException, GeneralSecurityException {
+    void successTestActual() throws IOException, GeneralSecurityException {
+
+        // input data + data access interface
+        VideoSearchInputData inputData = new VideoSearchInputData("H-v6DfxnjF8", "user");
+        VideoSearchDataAccessInterface youtubeRepository = new YouTubeDataAccess();
+
+        UserFactory factory = new CommonUserFactory();
+        FileUserDataAccessObject userRepository = new FileUserDataAccessObject("users.csv", factory);
+        VideoSearchUserDataAccessInterface historyRepository = new FileHistoryDataAccessObject(userRepository);
+
+        // presenter + success and fail view
+        VideoSearchOutputBoundary presenter = new VideoSearchOutputBoundary() {
+            @Override
+            public void prepareSuccessView(VideoSearchOutputData video) {
+                assertEquals("H-v6DfxnjF8", video.getVideoId());
+                assertEquals("FIE Fencing Channel", video.getChannelName());
+                assertEquals("Grand Prix Turin 2020 Women's Foil Final THIBUS FRA vs KIEFER USA", video.getTitle());
+                assertEquals("", video.getDescription());
+//                assertEquals("", video.getVideoPublishDate());
+                assertEquals(13013, video.getViewCount());
+                assertEquals(169, video.getLikeCount());
+                assertEquals(9, video.getCommentCount());
+
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("This use case should not fail.");
+            }
+
+        };
+
+        // interactor + execute
+        VideoSearchInputBoundary interactor = new VideoSearchInteractor(youtubeRepository, historyRepository, presenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void failureTestMock() throws IOException, GeneralSecurityException {
         VideoSearchInputData inputData = new VideoSearchInputData("123", "user");
         VideoSearchDataAccessInterface youtubeRepository = new YouTubeDataAccess();
         VideoSearchUserDataAccessInterface historyRepository = new InMemoryHistoryDataAccessObject();
@@ -75,5 +113,30 @@ class VideoSearchInteractorTest {
         interactor.execute(inputData);
 
     }
+
+    @Test
+    void failureTestActual() throws IOException, GeneralSecurityException {
+        VideoSearchInputData inputData = new VideoSearchInputData("123", "user");
+        VideoSearchDataAccessInterface youtubeRepository = new YouTubeDataAccess();
+        UserFactory factory = new CommonUserFactory();
+        FileUserDataAccessObject userRepository = new FileUserDataAccessObject("users.csv", factory);
+        VideoSearchUserDataAccessInterface historyRepository = new FileHistoryDataAccessObject(userRepository);
+        VideoSearchOutputBoundary presenter = new VideoSearchOutputBoundary() {
+            @Override
+            public void prepareSuccessView(VideoSearchOutputData video) {
+                fail("This use case should not pass.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Video ID does not exist.", error);
+            }
+        };
+
+        VideoSearchInputBoundary interactor = new VideoSearchInteractor(youtubeRepository, historyRepository, presenter);
+        interactor.execute(inputData);
+
+    }
+
 
 }
