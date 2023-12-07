8f1a041e7857c28449b938bc2a3358d173658910
jonathanjschen913 <jonathanchen913@gmail.com>
2023-12-06 19:40:36 -0500

    implementing controller strategy pattern

--- src/interface_adapter/compare_search/CompareSearchController.java
+++ src/interface_adapter/compare_search/CompareSearchController.java
@@ -1,12 +1,13 @@
 package interface_adapter.compare_search;
 
+import interface_adapter.SearchController;
 import use_case.compare_videos.CompareSearchInputBoundary;
 import use_case.compare_videos.CompareSearchInputData;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-public class CompareSearchController {
+public class CompareSearchController implements SearchController {
 
     final CompareSearchInputBoundary compareSearchUseCaseInteractor;
 
@@ -17,5 +18,10 @@ public class CompareSearchController {
     public void execute(String videoOneId, String videoTwoId)throws GeneralSecurityException, IOException {
         CompareSearchInputData compareSearchInputData = new CompareSearchInputData(videoOneId, videoTwoId);
         compareSearchUseCaseInteractor.execute(compareSearchInputData);
+    }
+
+    @Override
+    public void callExecute(String data, String username) throws GeneralSecurityException, IOException {
+
     }
 }
